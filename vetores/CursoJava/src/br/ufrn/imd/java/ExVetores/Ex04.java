f0997a583e9d19a7c3dd3306f34bce834f1602ac
Allan Rêgo <allan.rego@imd.local>
2019-06-03 19:00:35 -0300

    Exemplo 04 vetores. vetorB = sqrt(vetorA);

---
+++ vetores/CursoJava/src/br/ufrn/imd/java/ExVetores/Ex04.java
@@ -0,0 +1,33 @@
+package br.ufrn.imd.java.ExVetores;
+
+import java.util.Scanner;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		Scanner scan =  new Scanner(System.in);
+		
+		int[] vetorA = new int[5];
+		int[] vetorB = new int[vetorA.length];
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("entre com o valor da posição " + i);
+			vetorA[i] = scan.nextInt();
+		}
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			vetorB[i] = (int) Math.sqrt(vetorA[i]);
+		//System.out.println(vetorB[i] + " ");
+		}
+		System.out.println("Vetor A = " );
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println(vetorA[i] + " ");
+		}
+		System.out.println("vetor B = " );
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println(vetorB[i] + " ");
+		}
+		scan.close();
+	}
+
+}
